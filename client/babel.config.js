5e3b1f2d430b1d77f29c5a58382b44dbe18f4089
shun91 <shun9167@gmail.com>
2021-12-11 15:10:12 +0900

    ➕ add prettier

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,9 +1,3 @@
 module.exports = {
-  presets: [
-    ['@babel/preset-env'],
-    [
-      '@babel/preset-react',
-      { development: process.env.NODE_ENV !== 'production' },
-    ],
-  ],
+  presets: [['@babel/preset-env'], ['@babel/preset-react', { development: process.env.NODE_ENV !== 'production' }]],
 };
