4fccb8749105fd75108196800026e6bb7c22720f
shun91 <shun9167@gmail.com>
2021-12-15 06:50:39 +0900

    ➖ standardized-audio-contextを削除 ( #10 )

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -52,7 +52,6 @@ const config = {
   },
   plugins: [
     new webpack.ProvidePlugin({
-      AudioContext: ['standardized-audio-context', 'AudioContext'],
       Buffer: ['buffer', 'Buffer'],
     }),
     new webpack.EnvironmentPlugin({
